a98390e486b3f366a171d29a29e7706177fa03a0
benjiaming <ben@blazke.cz>
2019-04-27 15:08:42 -0700

    plus_one

--- plus_one.py
+++ plus_one.py
@@ -19,7 +19,7 @@ Output: [4,3,2,2]
 Explanation: The array represents the integer 4321.
 """
 class Solution(object):
-    def plusOneGolf(self, digits):
+    def plusOne(self, digits):
         """
         :type digits: List[int]
         :rtype: List[int]
