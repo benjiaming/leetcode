061ae06065daa40091226a2add06d69861a539df
benjiaming <ben@blazke.cz>
2019-04-30 22:06:09 -0700

    design_hashmap

---
+++ test_design_hashmap.py
@@ -0,0 +1,17 @@
+import unittest
+from design_hashmap import MyHashMap
+
+class TestSolution(unittest.TestCase):
+    def test_design_hashmap(self):
+        hashmap = MyHashMap()
+        hashmap.put(1, 1)
+        hashmap.put(2, 2)
+        self.assertEquals(hashmap.get(1), 1)
+        self.assertEqual(hashmap.get(3), -1) # not found
+        hashmap.put(2, 1)
+        self.assertEquals(hashmap.get(2), 1) # update the existing value
+        hashmap.remove(2)
+        self.assertEqual(hashmap.get(2), -1)
+
+if __name__ == '__main__':
+    unittest.main()
