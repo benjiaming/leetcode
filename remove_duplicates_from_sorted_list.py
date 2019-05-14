a765380ab677a45c64501e73d862502ffc7e53dc
benjiaming <ben@blazke.cz>
2019-05-14 10:59:08 -0700

    remove_duplicates_from_sorted_list

---
+++ remove_duplicates_from_sorted_list.py
@@ -0,0 +1,67 @@
+"""
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+Example 1:
+
+Input: 1->1->2
+Output: 1->2
+
+Example 2:
+
+Input: 1->1->2->3->3
+Output: 1->2->3
+"""
+#%%
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    @classmethod
+    def traverse(cls, head):
+        output = str(head.val)
+        if head.next:
+            output += '->' + cls.traverse(head.next)
+        return output
+
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """        
+        if not head:
+            return
+        node = head.next
+        prev = head
+        while node:
+            if node.val == prev.val:
+                prev.next = node.next
+            else:
+                prev = node
+            node = node.next
+        return head
+        
+head = ListNode(1)
+head.next = ListNode(1)
+head.next.next = ListNode(2)
+head.next.next.next = ListNode(2)
+head.next.next.next.next = ListNode(2)
+head.next.next.next.next.next = ListNode(3)
+head.next.next.next.next.next.next = ListNode(3)
+print(ListNode.traverse(head))
+solution.deleteDuplicates(head)
+print(ListNode.traverse(head))
+
+print()
+
+solution = Solution()
+head = ListNode(1)
+head.next = ListNode(1)
+head.next.next = ListNode(1)
+head.next.next.next = ListNode(1)
+print(ListNode.traverse(head))
+solution.deleteDuplicates(head)
+print(ListNode.traverse(head))
---
+++ test_remove_duplicates_from_sorted_list.py
@@ -0,0 +1,48 @@
+import unittest
+from remove_duplicates_from_sorted_list import Solution, ListNode
+
+class TestSolution(unittest.TestCase):
+    def test_remove_duplicates_from_sorted_list(self):
+        # 1->1->2 => 1->2
+        head = ListNode(1)
+        head.next = ListNode(1)
+        head.next.next = ListNode(2)
+        before = ListNode.traverse(head)
+        self.assertEqual(before, '1->1->2')
+
+        solution = Solution()
+        solution.deleteDuplicates(head)
+        after = ListNode.traverse(head)
+        self.assertEqual(after, '1->2')
+
+        # 1->1->2->3->3 => 1->2->3
+        head = ListNode(1)
+        head.next = ListNode(1)
+        head.next.next = ListNode(2)
+        head.next.next.next = ListNode(3)
+        head.next.next.next.next = ListNode(3)
+        before = ListNode.traverse(head)
+        self.assertEqual(before, '1->1->2->3->3')
+
+        solution = Solution()
+        solution.deleteDuplicates(head)
+        after = ListNode.traverse(head)
+        self.assertEqual(after, '1->2->3')
+
+        # 1->1->1->1 => 1
+        head = ListNode(1)
+        head.next = ListNode(1)
+        head.next.next = ListNode(1)
+        head.next.next.next = ListNode(1)
+        head.next.next.next.next = ListNode(1)
+        before = ListNode.traverse(head)
+        self.assertEqual(before, '1->1->1->1->1')
+
+        solution = Solution()
+        solution.deleteDuplicates(head)
+        after = ListNode.traverse(head)
+        self.assertEqual(after, '1')
+
+
+if __name__ == '__main__':
+    unittest.main()
