865ac20e276039a52e789d5f10dfd78af9cb7793
benjiaming <ben@blazke.cz>
2019-05-13 17:30:53 -0700

    rotate_image

---
+++ rotate_image.py
@@ -0,0 +1,81 @@
+#!/bin/env python3
+"""
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Note:
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+
+Given input matrix = 
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+rotate the input matrix in-place such that it becomes:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+
+Example 2:
+
+Given input matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+], 
+
+rotate the input matrix in-place such that it becomes:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+"""
+#%%
+from typing import List
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+
+        Runs in O(n^2).
+        """
+        lm = len(matrix)
+        if not lm or lm != len(matrix[0]):
+            return
+        for r in range(lm//2):
+            first = r
+            last = lm - 1 - r
+            for i in range(first, last):
+                offset = i - first
+                top = matrix[first][i]
+
+                matrix[first][i] = matrix[last-offset][first]
+                matrix[last-offset][first] = matrix[last][last-offset]
+                matrix[last][last-offset] = matrix[i][last]
+                matrix[i][last] = top
+
+solution = Solution()
+
+input = [
+    [ 5, 1, 9,11],
+    [ 2, 4, 8,10],
+    [13, 3, 6, 7],
+    [15,14,12,16]
+]
+solution.rotate(input)
+print(input)
+
+#%%
